e79e3cc23c7de68fef3e65829c3f5b0d05cd3da8
weblerson <zonejr4@gmail.com>
2024-01-20 18:31:28 -0400

    test <products.tests>: created review serializer tests

--- apps/products/tests/__init__.py
+++ apps/products/tests/__init__.py
@@ -2,3 +2,4 @@ from .product_serializer_tests import ProductSerializerTests
 from .supplier_serializer_tests import SupplierSerializerTests
 from .tag_serializer_tests import TagSerializerTests
 from .price_history_serializer_tests import PriceHistorySerializerTests
+from .review_serializer_tests import ReviewSerializerTests
---
+++ apps/products/tests/review_serializer_tests.py
@@ -0,0 +1,119 @@
+from rest_framework import test
+
+from ..models import Review, Supplier, Product
+from authentication.models import Customer
+
+from ..serializers import ReviewSerializer
+
+
+class ReviewSerializerTests(test.APITestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        test_supplier_data = {
+            'name': 'TestCia',
+            'address': 'TestStreet',
+            'phone': '99999999999'
+        }
+
+        cls.fields = ('product', 'customer', 'value')
+
+        supplier: Supplier = Supplier.objects.create(**test_supplier_data)
+
+        test_product_data = {
+            'name': 'Test Science Product',
+            'description': 'Test Description',
+            'category': 1,
+            'supplier': supplier
+        }
+
+        product: Product = Product.objects.create(**test_product_data)
+
+        test_customer_data = {
+            'username': 'testuser',
+            'email': 'test@test.dev',
+            'password': 'testpassword'
+        }
+
+        customer: Customer = Customer.objects.create(**test_customer_data)
+
+        cls.test_data = {
+            'product': product.id,
+            'customer': customer.id,
+            'value': 4.5
+        }
+
+        cls.update_test_data = {
+            'value': 5.0
+        }
+
+    @staticmethod
+    def __create_review_instance(review_data):
+        serializer: ReviewSerializer = ReviewSerializer(data=review_data)
+        serializer.is_valid()
+
+        instance: Review = serializer.save()
+
+        return instance
+
+    def test_if_review_serializer_have_all_fields(self):
+        """
+        Tests if review serializer have all fields (product, customer and value)
+        """
+
+        serializer: ReviewSerializer = ReviewSerializer()
+
+        for field in self.fields:
+            self.assertIn(field, serializer.fields)
+
+    def test_if_review_serializer_can_create_a_review(self):
+        """
+        Tests if review serializer can create a review
+        """
+
+        serializer: ReviewSerializer = ReviewSerializer(data=self.test_data)
+        serializer.is_valid()
+
+        instance: Review = serializer.save()
+
+        created = Review.objects.filter(product=instance.product.id).filter(customer=instance.customer.id)
+
+        self.assertTrue(created.exists())
+        self.assertEqual(created.first().value, self.test_data.get('value'))
+
+    def test_if_review_serializer_can_update_a_review_value(self):
+        """
+        Tests if review serializer can update a review's value
+        """
+
+        instance: Review = self.__create_review_instance(self.test_data)
+        serializer: ReviewSerializer = ReviewSerializer()
+        serializer.update(instance, self.update_test_data)
+
+        updated: Review = Review.objects.get(product=instance.product, customer=instance.customer)
+
+        self.assertEqual(updated.value, self.update_test_data.get('value'))
+
+    def test_if_review_serializer_returns_the_correct_json(self):
+        """
+        Tests if review serializer returns the correct JSON
+        """
+
+        instance: Review = self.__create_review_instance(self.test_data)
+
+        expected_json = {
+            'product': instance.product.id,
+            'customer': instance.customer.id,
+            'value': instance.value
+        }
+
+        serializer: ReviewSerializer = ReviewSerializer(instance=instance)
+
+        self.assertDictEqual(expected_json, serializer.data)
+
+    def test_if_the_average_review_changes_automatically_when_another_review_instance_is_created(self):
+        """
+        Tests if the average review of a product changes automatically when another review instance is created
+        """
+
+        ...
