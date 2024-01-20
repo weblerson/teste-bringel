81792eaa08d2ce292c497afeadf61c8bf8148451
weblerson <zonejr4@gmail.com>
2024-01-20 17:27:50 -0400

    feat <products.models>: now 'end' property from PriceHistory model can be nullable

--- apps/products/models/price_history.py
+++ apps/products/models/price_history.py
@@ -6,4 +6,4 @@ class PriceHistory(models.Model):
     product = models.ForeignKey('products.Product', on_delete=models.CASCADE, blank=False)
     price = models.IntegerField()
     start = models.DateField(auto_now=True)
-    end = models.DateField()
+    end = models.DateField(null=True)
