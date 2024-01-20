e48f0a8e98ed33499b31d3a2726672c87d81c6a0
weblerson <zonejr4@gmail.com>
2024-01-20 17:54:22 -0400

    feat <products.serializers>: created review serializer

---
+++ apps/products/serializers/review_serializer.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from ..models import Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Review
+        fields = ['product', 'customer', 'value']
