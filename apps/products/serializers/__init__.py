984e9638f2cf1dc277d3041b345711fb0ddf192d
weblerson <zonejr4@gmail.com>
2024-01-20 15:37:17 -0400

    feat <products.serializers>: created price history serializer

--- apps/products/serializers/__init__.py
+++ apps/products/serializers/__init__.py
@@ -1,3 +1,4 @@
 from .product_serializer import ProductSerializer
 from .supplier_serializer import SupplierSerializer
 from .tag_serializer import TagSerializer
+from .price_history_serializer import PriceHistorySerializer
---
+++ apps/products/serializers/price_history_serializer.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+
+from django.utils import timezone
+
+from ..models import PriceHistory, Product
+
+
+class PriceHistorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PriceHistory
+        fields = ['id', 'product', 'price', 'start', 'end']
+
+    def create(self, validated_data):
+        product: Product = validated_data.get('product')
+
+        price_history = PriceHistory.objects.filter(product=product).order_by('start')
+        if price_history.exists():
+            last: PriceHistory = price_history.last()
+            last.end = timezone.now()
+            last.save()
+
+        super().create(validated_data)
