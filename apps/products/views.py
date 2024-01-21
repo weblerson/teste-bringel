a2bc7cdbd6fb3518f536b96281ce607c218927b5
weblerson <zonejr4@gmail.com>
2024-01-21 12:52:06 -0400

    feat <products.views>: changed products viewsets permissions

--- apps/products/views.py
+++ apps/products/views.py
@@ -7,8 +7,6 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
 
-from django.db.models import Count
-
 from .models import Product, Supplier, Tag, PriceHistory, Review
 from .serializers import (ProductSerializer,
                           SupplierSerializer,
@@ -22,22 +20,54 @@ class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    def get_permissions(self):
+        match self.action:
+            case 'list' | 'retrieve':
+                permission_classes = [permissions.AllowAny]
+
+            case _:
+                permission_classes = [permissions.IsAdminUser]
+
+        return [permission() for permission in permission_classes]
+
 
 class SupplierViewSet(viewsets.ModelViewSet):
     queryset = Supplier.objects.all()
     serializer_class = SupplierSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    def get_permissions(self):
+        match self.action:
+            case 'list' | 'retrieve':
+                permission_classes = [permissions.AllowAny]
+
+            case _:
+                permission_classes = [permissions.IsAdminUser]
+
+        return [permission() for permission in permission_classes]
+
 
 class TagViewSet(mixins.ListModelMixin,
                  mixins.CreateModelMixin,
-                 mixins.RetrieveModelMixin,
                  mixins.DestroyModelMixin,
                  viewsets.GenericViewSet):
 
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get_permissions(self):
+        match self.action:
+            case 'list':
+                permission_classes = [permissions.AllowAny]
+
+            case 'destroy':
+                permission_classes = [permissions.IsAdminUser]
+
+            case _:
+                permission_classes = [permissions.IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
 
 
 class PriceHistoryViewSet(mixins.ListModelMixin,
@@ -50,9 +80,20 @@ class PriceHistoryViewSet(mixins.ListModelMixin,
     serializer_class = PriceHistorySerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    def get_permissions(self):
+        match self.action:
+            case 'list' | 'retrieve':
+                permission_classes = [permissions.AllowAny]
+
+            case _:
+                permission_classes = [permissions.IsAdminUser]
+
+        return [permission() for permission in permission_classes]
+
 
 class ReviewViewSet(mixins.ListModelMixin,
                     mixins.CreateModelMixin,
+                    mixins.UpdateModelMixin,
                     mixins.RetrieveModelMixin,
                     mixins.DestroyModelMixin,
                     viewsets.GenericViewSet):
@@ -60,6 +101,16 @@ class ReviewViewSet(mixins.ListModelMixin,
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get_permissions(self):
+        match self.action:
+            case 'list' | 'retrieve':
+                permission_classes = [permissions.AllowAny]
+
+            case _:
+                permission_classes = [permissions.IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
 
 
 class RecommendationAlgorithmViewSet(viewsets.GenericViewSet):
