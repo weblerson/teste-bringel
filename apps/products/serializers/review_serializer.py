e65172b9c5a040c1eee443ccee2042bda17ce30d
weblerson <zonejr4@gmail.com>
2024-01-21 00:00:43 -0400

    feat <products.serializers>: now review serializer calls the update_product_average_review task on create

--- apps/products/serializers/review_serializer.py
+++ apps/products/serializers/review_serializer.py
@@ -8,3 +8,11 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = ['product', 'customer', 'value']
+
+    def create(self, validated_data):
+        instance: Review = super().create(validated_data)
+
+        from ..tasks import update_product_average_review
+        update_product_average_review.delay(instance.product.id)
+
+        return instance
