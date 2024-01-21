5cdce17f21df7cc4d0ce20a025f3a26ffcbf927d
weblerson <zonejr4@gmail.com>
2024-01-21 00:25:56 -0400

    feat <products.views>: created products viewsets

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,5 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from . import views
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'products', views.ProductViewSet, basename='product')
+router.register(r'suppliers', views.SupplierViewSet, basename='supplier')
+router.register(r'tags', views.TagViewSet, basename='tag')
+router.register(r'prices', views.PriceHistoryViewSet, basename='price')
+router.register(r'reviews', views.ReviewViewSet, basename='review')
 
 urlpatterns = [
-
+    path('', include(router.urls))
 ]
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework import mixins
 
-# Create your views here.
+from .models import Product, Supplier, Tag, PriceHistory, Review
+from .serializers import (ProductSerializer,
+                          SupplierSerializer,
+                          TagSerializer,
+                          PriceHistorySerializer,
+                          ReviewSerializer)
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class SupplierViewSet(viewsets.ModelViewSet):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class TagViewSet(mixins.ListModelMixin,
+                 mixins.CreateModelMixin,
+                 mixins.RetrieveModelMixin,
+                 mixins.DestroyModelMixin,
+                 viewsets.GenericViewSet):
+
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class PriceHistoryViewSet(mixins.ListModelMixin,
+                          mixins.CreateModelMixin,
+                          mixins.RetrieveModelMixin,
+                          mixins.DestroyModelMixin,
+                          viewsets.GenericViewSet):
+
+    queryset = PriceHistory.objects.all()
+    serializer_class = PriceHistorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ReviewViewSet(mixins.ListModelMixin,
+                    mixins.CreateModelMixin,
+                    mixins.RetrieveModelMixin,
+                    mixins.DestroyModelMixin,
+                    viewsets.GenericViewSet):
+
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticated]
