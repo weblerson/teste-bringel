1be1263a3da5fc54b25d554e620e89561373485f
weblerson <zonejr4@gmail.com>
2024-01-23 14:07:28 -0400

    fix <cart.models>: fixed translations gettext_lazy exception for __str__

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext as _
 
 from authentication.models import Customer
 from products.models import Product
