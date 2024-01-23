090873ad1f99e11ed56a82bb93aa26800379bf8a
weblerson <zonejr4@gmail.com>
2024-01-23 03:52:02 -0400

    feat <cart.models>: changed cart model

--- apps/cart/serializers.py
+++ apps/cart/serializers.py
@@ -1,13 +1,23 @@
 from rest_framework import serializers
 
 from . import models
+from products.models import Product
+
+
+class CartRequestSerializer(serializers.ModelSerializer):
+
+    product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
+
+    class Meta:
+        model = models.Cart
+        fields = ['customer', 'product']
 
 
 class CartSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Cart
-        fields = ['id', 'customer', 'products']
+        fields = ['id', 'customer', 'product']
 
 
 class SaleSerializer(serializers.ModelSerializer):
