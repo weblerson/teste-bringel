57daa902820cbc148854a52d5ef272dc1e4df358
weblerson <zonejr4@gmail.com>
2024-01-23 01:42:23 -0400

    feat <cart.models>: created cart models

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
+
+from authentication.models import Customer
+from products.models import Product
+
+
+class Cart(models.Model):
+
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+
+    def __str__(self) -> str:
+        return _(f'Cart of {self.customer.username}')
+
+
+class Sale(models.Model):
+
+    class Payment(models.IntegerChoices):
+        PIX = 1, _('Pix'),
+        CREDIT = 2, _('Credit')
+        DEBIT = 3, _('Debit')
+
+    customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
+    products = models.ManyToManyField(Product)
+
+    total = models.IntegerField()
+    delivery_address = models.CharField(max_length=64)
+    payment_method = models.IntegerField(choices=Payment)
+    date = models.DateField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f'Sold to {self.customer.username} at {self.date}'
