7bddaecd0b861a8c70c0a0f1a1fa5af7f457e806
weblerson <zonejr4@gmail.com>
2024-01-23 14:06:33 -0400

    feat <products.admin>: created products models' admin

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.utils.translation import gettext_lazy as _
+
+from . import models
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    class CustomerInlineAdmin(admin.TabularInline):
+        model = models.Product.customers.through
+        readonly_fields = ('customer', 'product', 'value')
+        can_delete = False
+
+    fieldsets = (
+        (_('Product Info'), {'fields': ('name', 'description', 'sku', 'category', 'average_review')}),
+        (_('Supplier'), {'fields': ('supplier',)}),
+    )
+
+    readonly_fields = ('sku', 'average_review')
+
+    inlines = (CustomerInlineAdmin,)
+
+
+@admin.register(models.Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+
+    fieldsets = (
+        (_('Supplier Info'), {'fields': ('name', 'address', 'phone')}),
+    )
+
+
+@admin.register(models.Tag)
+class TagAdmin(admin.ModelAdmin):
+
+    fieldsets = (
+        (_('Tag Info'), {'fields': ('name', 'product')}),
+    )
+
+
+@admin.register(models.Review)
+class ReviewAdmin(admin.ModelAdmin):
+
+    fieldsets = (
+        (_('Review Info'), {'fields': ('product', 'customer', 'value')}),
+    )
+
+    readonly_fields = ('product', 'customer', 'value')
+
+
+@admin.register(models.PriceHistory)
+class PriceHistoryAdmin(admin.ModelAdmin):
+
+    fieldsets = (
+        (_('Price History Info'), {'fields': ('product', 'price', 'start', 'end')}),
+    )
+
+    readonly_fields = ('start', 'end')
