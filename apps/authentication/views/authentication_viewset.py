acf4e1731945fc954a42a165853b4536b856b69d
weblerson <zonejr4@gmail.com>
2024-01-24 20:29:03 -0400

    doc <swagger>: created serializers for success token responses

--- apps/authentication/views/authentication_viewset.py
+++ apps/authentication/views/authentication_viewset.py
@@ -4,6 +4,7 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import permissions
+from rest_framework import serializers
 
 from rest_framework.decorators import action
 
@@ -11,7 +12,27 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 from oauth2_provider.contrib.rest_framework.authentication import OAuth2Authentication
 
+from drf_spectacular.utils import extend_schema, extend_schema_view, inline_serializer
 
+
+@extend_schema_view(
+    generate_jwt_token_for_customer=extend_schema(
+        responses={
+            status.HTTP_200_OK: inline_serializer(
+                name='InlineJwtTokenSerializer',
+                fields={
+                    'access': serializers.CharField(default='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxNCIsIm5h'
+                                                            'bWUiOiJsZXJzaW5obyBkYSBzaWx2YSBhbWlndXJ1bWkiLCJpYXQiOjE1MT'
+                                                            'YyMzkwMjJ9.nC09Da53OMLQdIOrbkeYNTpyYVDaZlbAjyBC7rN2M3E'),
+
+                    'refresh': serializers.CharField(default='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxNCIsIm5h'
+                                                             'bWUiOiJuaW5hIGRhIHNpbHZhIGFtaWd1cnVtaSIsImlhdCI6MTUxNjIzO'
+                                                             'TAyMn0.race4dBfOvg3L_1AdoK8ewUYEqkq_2ZwzXvhoU5LO-M')
+                }
+            )
+        }
+    )
+)
 class AuthenticationViewSet(viewsets.ViewSet):
 
     authentication_classes = [OAuth2Authentication]
--- apps/authentication/views/token_api_view.py
+++ apps/authentication/views/token_api_view.py
@@ -3,6 +3,7 @@ from oauth2_provider.views import TokenView
 
 from rest_framework import serializers
 from rest_framework import views
+from rest_framework import status
 
 
 class TokenApiView(TokenView, views.APIView):
@@ -16,7 +17,19 @@ class TokenApiView(TokenView, views.APIView):
                 "client_secret": serializers.CharField(),
                 "client_id": serializers.CharField(),
             },
-        )
+        ),
+        responses={
+            status.HTTP_200_OK: inline_serializer(
+                name='InlineResponseTokenSerializer',
+                fields={
+                    'access_token': serializers.CharField(default='lSUruikOljhghYBNjasd85P8Yubgh9'),
+                    'expires_in': serializers.IntegerField(default=36000),
+                    'token_type': serializers.CharField(default='Bearer'),
+                    'scope': serializers.CharField(default='read write groups'),
+                    'refresh_token': serializers.CharField(default='jsuTGb56Tgak8pTkshgf5olYncjsuo')
+                }
+            )
+        }
     )
     def post(self, request, *args, **kwargs):
         return super().post(request, *args, **kwargs)
